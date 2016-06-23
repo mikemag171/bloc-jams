7eb634dd2d4e6f229396c95448efb29f180b66c6
Mike <magnussenmike@gmail.com>
2016-06-23 13:07:52 -0500

    bloc32commit2

--- scripts/album.js
+++ scripts/album.js
@@ -1,51 +1,3 @@
-var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
- 
-var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
-
-//???? Is the above set of titles/duration an object containing an array? To confirm, an object can contain an array?
-//
-
-var albumColdplay = {
-     title: 'Mylo Xyloto',
-     artist: 'Coldplay',
-     label: 'Atlantic',
-     year: '2009',
-     albumArtUrl: 'assets/images/album_covers/mylo_xyloto.jpg',
-     songs: [
-         { title: 'Hurts Like Heaven', duration: '4:02' },
-         { title: 'Paradise', duration: '4:38' },
-         { title: 'Every Teardrop is a Waterfall', duration: '4:01'},
-         { title: 'A Hopeful Transmission', duration: '0:33' },
-         { title: 'Up With The Birds', duration: '3:48'}
-     ]
- };
-
 //createSongRom is a variable that contains the html template through which the album objects' info can be passed.
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
@@ -113,6 +65,7 @@ var clickHandler = function() {
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
+     currentAlbum = album;
      // #2
      $albumTitle.text(album.title);
      $albumArtist.text(album.artist);
@@ -132,7 +85,9 @@ var setCurrentAlbum = function(album) {
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-var currentlyPlayingSong = null;
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
