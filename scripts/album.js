a55c07813c19abea77d934419fc38f454fed0116
Mike <magnussenmike@gmail.com>
2016-06-30 10:23:52 -0500

    togglePlayFromPlayerbar selector variable, click event, and function added

--- scripts/album.js
+++ scripts/album.js
@@ -195,6 +195,19 @@ var previousSong = function() {
     
 };
 
+var togglePlayFromPlayerbar = function() {
+  var $currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+  if (currentSoundFile.isPaused()) {
+    $currentlyPlayingCell.html(pauseButtonTemplate);
+    $(this).html(playerBarPauseButton);
+    currentSoundFile.play();
+    } else if (currentSoundFile) {
+    $currentlyPlayingCell.html(playButtonTemplate);
+      $(this).html(playerBarPlayButton);
+      currentSoundFile.pause();
+    }
+};
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -207,11 +220,13 @@ var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playPauseButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
   setCurrentAlbum(albumPicasso);
   $previousButton.click(previousSong);
   $nextButton.click(nextSong);
+  $playPauseButton.click(togglePlayFromPlayerbar);
 
     var albums = [albumPicasso, albumMarconi, albumColdplay];
     var index = 1;
